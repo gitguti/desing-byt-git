9428234e1ec34b82cbc20cf39cd0ff00eb978546
gitguti <gitmelgtrrz1@gmail.com>
2023-06-30 20:59:08 -0400

    update hero typewritter effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,64 @@ import Sticky from 'react-stickynode';
 import data from "../data.json";
 
 export default function Home() {
+
+  var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
   const information = { data };
   // console.log(information.data.data);
   const projects = information.data.data.projects;
@@ -39,15 +97,18 @@ export default function Home() {
       </Sticky>;
       <main className={styles.main__container}>
         <div className={styles.main__container__left}>
-          <h1 className={styles.main__container__left__title}>
-            {information.data.data.name}
-          </h1>
-          <h2 className={styles.main__container__left__subtext}>
-            {information.data.data.profession}
+        <h2 className={styles.main__container__left__greeting}>
+            {information.data.data.greetings}
           </h2>
-          <p className={styles.main__container__left__text}>
+          <h1 className={styles.main__container__left__title}>
+          <a href="" class="typewrite" data-period="2000" data-type='[ "Gitmel Gutiérrez", "Just call me Git😉", "@gitguti everywhere", "@whynotgit on twitter"]'>
+    <span class="wrap"></span>
+  </a>
+  </h1>
+
+          <h2 className={styles.main__container__left__subtext}>
             {information.data.data.objetive}
-          </p>
+          </h2>
           <Link
             to="/About"
             target="_self"
